c24be63322fa33292283690d093c7fe07ac8c31a
余宝彬 <502275531@qq.com>
2017-07-25 14:59:53 +0800

    update config info

--- demo/app/js/index.js
+++ demo/app/js/index.js
@@ -1,3 +1,4 @@
 /**
  * Created by yohouakira on 2017/7/5.
  */
+console.log(123)
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -7,12 +7,14 @@ const jsPath = path.resolve(__dirname,'./app/js/');
 const port = 8085;
 let config = {
 	entry: {
-	  app: [ path.resolve(jsPath,'index') ], // 用数组，app名字包含多个js
+	  index: path.resolve(jsPath, 'index'), // 用数组，app名字包含多个js
+    page: path.resolve(jsPath, 'page'),
 	  // app: 'index' //只包含一个
 	},
 	output: {
 	  path: path.resolve(__dirname,'dist'),
-	  filename: '[name].js'
+	  filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 	},
 	plugins: [
 	  //重复的代码打包
@@ -21,12 +23,12 @@ let config = {
 	    filename: "commons.js",
 	    minChunks: 2,
 	  }),
-      new ExtractTextPlugin("styles.css"), //将css，less打包成styles.css
-      new webpack.ProvidePlugin({ //全局配置加载
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery"
-      }),
+    new ExtractTextPlugin("styles.css"), //将css，less打包成styles.css
+    new webpack.ProvidePlugin({ //全局配置加载
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    }),
 	],
 	module: {
 	  rules:[{
@@ -62,18 +64,18 @@ let config = {
 	},
 	resolve: {
 	  //自动扩展文件后缀，在使用require时省略后缀名
-      extensions: ['.js', '.json', '.jsx', '.vue','.scss'],
-      alias: {
-    	'style': path.resolve(__dirname,'./app/styles/'),
-    	'view': path.resolve(__dirname,'./app/view')
-      }
+    extensions: ['.js', '.json', '.jsx', '.vue','.scss'],
+    alias: {
+  	  'style': path.resolve(__dirname,'./app/styles/'),
+  	  'view': path.resolve(__dirname,'./app/view')
+    }
 	},
 	// 服务器配置相关，自动刷新!
-    devServer: {
-      historyApiFallback: true,
-      inline: true,
-      port:port
-    }
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    port:port
+  }
 }
 const confTitle = [
 	{name: 'index', title: 'index',path: path.resolve(__dirname,'./app/view/index.html'), js: 'index'},
@@ -83,15 +85,15 @@ const confTitle = [
 confTitle.forEach(function(pathname) {
     var conf = {
     	title: pathname.name,
-		filename: pathname.name + '.html', //生成的html存放路径,相对于path
-		template: pathname.path, //html模板路径
-		inject: true, //允许插件修改哪些内容,包括head与body
-		hash: false, //是否添加hash值
-		minify: { //压缩HTML文件
-		  removeComments: true,//移除HTML中的注释
-		  collapseWhitespace: false //删除空白符与换行符
-        },
-        chunks:[pathname.js], //编译的时候需要引入entry中制定的js
+		  filename: pathname.name + '.html', //生成的html存放路径,相对于path
+		  template: pathname.path, //html模板路径
+		  inject: true, //允许插件修改哪些内容,包括head与body
+		  hash: false, //是否添加hash值
+		  minify: { //压缩HTML文件
+		    removeComments: true,//移除HTML中的注释
+		    collapseWhitespace: false //删除空白符与换行符
+      },
+      chunks:[pathname.js], //编译的时候需要引入entry中制定的js
     };
     config.plugins.push(new HtmlWebpackPlugin(conf));
 });
